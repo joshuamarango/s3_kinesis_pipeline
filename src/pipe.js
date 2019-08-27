2d0cdb5c53bfae1d0ffb1537af3e9cc9c998dbfa
joshuamarango <joshua@sarenoapp.com>
2019-08-27 16:25:00 +0100

    pipeline v1 complete

--- src/pipe.js
+++ src/pipe.js
@@ -17,9 +17,13 @@ if (cluster.isMaster) {
     for (let i = 0; i < numCPUs; ++i) {
         cluster.fork();
     }
-} else if(cluster.worker.id == 1){ batch_1() }
-  else if(cluster.worker.id == 2){ batch_2() }
-  else if(cluster.worker.id == 3){ batch_3() }
-  else if(cluster.worker.id == 4){ batch_4() }
-  else if(cluster.worker.id == 5){ batch_5() }
-  else if(cluster.worker.id == 6){ batch_6() }
+} else if(cluster.worker.id == 1){
+    batch_1();
+    batch_2();
+    batch_3();
+}
+  else if(cluster.worker.id == 2){
+    batch_4();
+    batch_5();
+    batch_6();
+}
